c2fb968a9d70be953184f68fd931d6e11cd9b005
colyst <mehmetecck@gmail.com>
2025-09-28 00:48:31 +0200

    v1

---
+++ index.js
@@ -0,0 +1,31 @@
+// index.js
+require('dotenv').config();
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent // <- like the actual part for the bot lol
+  ]
+});
+
+client.once('ready', () => {
+  console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.on('messageCreate', message => {
+  // ignore messages from bots
+  if (message.author.bot) return;
+
+  // normalize
+  const content = message.content.trim().toLowerCase();
+
+  if (content === 'nigga') {
+    // message.reply -> mentions the user
+    // message.channel.send does not
+    message.channel.send('i forgive u 🙏');
+  }
+});
+
+client.login(process.env.DISCORD_TOKEN);
